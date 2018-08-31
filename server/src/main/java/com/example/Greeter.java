2d3cbef98c710883028f29dffe4b4dec2ae10cea
Jeff <jeff@inspirii.com>
2018-08-31 11:14:03 +0000

    fix checks3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  /** @param someone name, 
-   *  @return - greeting string} 
+  /** @param someone name,
+   *  @return - greeting string}
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
